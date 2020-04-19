5468bfc9f8cbaa26b1dd36b07ffe8a3299eef0bc
laggu <sebastian.ra@groundx.xyz>
2020-04-19 17:17:00 +0900

    add post model

---
+++ server/models/posts.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+    id: mongoose.Schema.Types.ObjectId,
+    postedBy: mongoose.Schema.Types.ObjectId,
+    content: String,
+    visibility: { type: Number, default: 1 },
+},
+{
+  timestamps: true
+});
+
+const Post = mongoose.model('Post', postSchema);
+
+export default Post;
