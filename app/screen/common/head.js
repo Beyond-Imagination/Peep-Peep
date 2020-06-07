0d6ea9bf0b4c8c55fa0c0743780c4205cccdb5e6
laggu <sebastian.ra@groundx.xyz>
2020-06-07 19:10:58 +0900

    fix default flex

--- app/screen/common/head.js
+++ app/screen/common/head.js
@@ -1,20 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
-const Head = () => (
-    <View style={styles.head}>
+const Head = (props) => (
+    <View style={props.style}>
         <Text>Peep Peep</Text>
     </View>
 )
-
-const styles = StyleSheet.create({
-    head: {
-        flex: 1,
-        width: "100%",
-        backgroundColor: '#ffe277',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
 
 export default Head;
