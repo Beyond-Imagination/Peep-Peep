8e1a454414c522e9d078676493ca9c11bfd69d4a
Kuwon Sebastian Ra <laggu91@gmail.com>
2020-06-07 19:17:02 +0900

    fix default flex (#12)

--- app/screen/common/default.js
+++ app/screen/common/default.js
@@ -6,27 +6,23 @@ import Body from './body'
 
 // TODO: laggu - 추후 defaultSceen 이름 수정
 const defaultScreen = () => (
-    <View >
-      <Head style={styles.head}/>
-      <Body style={styles.body}/>
+    <View style={{flex: 1}}>
+        <Head style={styles.head}/>
+        <Body style={styles.body}/>
     </View>
 )
 
-// TODO: laggu - flex 레이아웃 문제 수정, head body 비율 맞추기
 const styles = StyleSheet.create({
     head: {
         flex: 1,
-        width: "100%",
         backgroundColor: '#ffe277',
         alignItems: 'center',
         justifyContent: 'center',
     },
     body: {
-        flex: 8,
+        flex: 9,
         backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
     },
-  });
+});
   
 export default defaultScreen;
