c1ec4bb9f5cdb9225e87edd0e014a627dfacce40
Kuwon Sebastian Ra <laggu91@gmail.com>
2020-06-14 23:27:12 +0900

    add stack navigator for signin, signup screen (#13)

---
+++ app/screen/signup/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+const SignUpScreen = () => (
+  <View style={styles.container}>
+    <Text>signup</Text>
+  </View>
+)
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+  
+export default SignUpScreen;
