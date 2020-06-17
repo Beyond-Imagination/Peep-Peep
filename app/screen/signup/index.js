b02a5f22c8cb3f84258a1caddf4f58aeb72a01de
laggu <sebastian.ra@groundx.xyz>
2020-06-17 23:47:53 +0900

    add signup screen

--- app/screen/signup/index.js
+++ app/screen/signup/index.js
@@ -1,19 +1,98 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React, {Component} from 'react';
+import { StyleSheet, View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
-const SignUpScreen = () => (
-  <View style={styles.container}>
-    <Text>signup</Text>
-  </View>
-)
+export default class SignUpScreen extends Component{
+    
+    static navigationOptions = {
+        header: null,
+    };
+
+    _goLogin(){
+        this.props.navigation.replace('SignIn')
+    }
+
+    _doSignUp() {
+        this.props.navigation.replace('TabNavigator')
+    }
+
+    render(){
+        return (
+            <View style={styles.container}>
+                <View style={styles.titleArea}>
+                    <Text style={styles.title}>Peep Peep</Text>
+                </View>
+                <View style={styles.formArea}>
+                    <TextInput 
+                        style={styles.textForm} 
+                        placeholder={"ID"}/>
+                    <TextInput 
+                        style={styles.textForm} 
+                        placeholder={"Password"}/>
+                    <TextInput 
+                        style={styles.textForm} 
+                        placeholder={"Nickname"}/>
+                </View>
+                <View style={styles.buttonArea}>
+                    <TouchableOpacity 
+                        style={styles.button}
+                        onPress={this._doSignUp.bind(this)}>
+                        <Text style={styles.buttonTitle}>Sign Up</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity 
+                        style={styles.button}
+                        onPress={this._goLogin.bind(this)}>
+                        <Text style={styles.buttonTitle}>Cancel</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        );
+    }
+}
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-  
-export default SignUpScreen;
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+        paddingLeft: wp('10%'),
+        paddingRight: wp('10%'),
+        justifyContent: 'center',
+    },
+    titleArea: {
+        width: '100%',
+        padding: wp('10%'),
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: wp('10%'),
+    },
+    formArea: {
+        width: '100%',
+        paddingBottom: wp('10%'),
+    },
+    textForm: {
+        borderWidth: 0.5,
+        borderColor: '#888',
+        width: '100%',
+        height: hp('5%'),
+        paddingLeft: 5,
+        paddingRight: 5,
+        marginBottom: 5,
+    },
+    buttonArea: {
+        width: '100%',
+        height: hp('5%'),
+        flexDirection: 'row',
+    },
+    button: {
+        backgroundColor: "#ffe277",
+        width: "40%",
+        height: "100%",
+        margin: "5%",
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    buttonTitle: {
+        color: 'white',
+    },
+})
