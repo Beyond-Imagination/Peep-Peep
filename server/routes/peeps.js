5715c028f712fbbfa738491ba4485b242feec02c
laggu <sebastian.ra@groundx.xyz>
2020-06-21 23:22:44 +0900

    add get api for peep

--- server/routes/peeps.js
+++ server/routes/peeps.js
@@ -1,19 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
-const Post = require("../models/peeps");
+const Peep = require("../models/peeps");
 
 router.post('/', function(req, res, next){
-    newPeep = new Post();
+    newPeep = new Peep();
     newPeep.content = req.body.content;
 
     newPeep.save(function(err) {
         if (err) {
-            res.json({"error": err})
+            res.json({"error": err});
+            return;
         }
+        res.json({"success": true});
     });
+});
 
-    res.json({"success": true})
+router.get('/', function(req, res, next){
+    Peep.find(function(err, peeps){
+        if(err) 
+            return res.status(500).send({error: 'fail to read database'});
+        res.json(peeps);
+    })
 });
 
 module.exports = router;
