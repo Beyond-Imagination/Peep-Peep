58126a1ae77e2ec6984df41a22ad9444021670df
Kuwon Sebastian Ra <laggu91@gmail.com>
2020-06-26 00:20:28 +0900

    add follow route (#25)

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const passport = require('../passport');
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/signup', passport.authenticate('local-signup',{
+  successRedirect : "/",
+  failureRedirect : "/user/signup/fail",
+}));
+
+router.post('/signin', passport.authenticate('local-signin', {
+  successRedirect : "/",
+  failureRedirect : "/signin",
+}))
 
 module.exports = router;
