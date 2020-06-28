f0723c04fdee52a6029970d238e881bc5cd81544
Kuwon Sebastian Ra <laggu91@gmail.com>
2020-06-28 16:42:56 +0900

    use jwt for peep api (#30)

--- server/routes/peeps.js
+++ server/routes/peeps.js
@@ -1,28 +1,47 @@
 const express = require('express');
 const router = express.Router();
 
+const passport = require("../passport")
 const Peep = require("../models/peeps");
 
 router.post('/', function(req, res, next){
-    newPeep = new Peep();
-    newPeep.content = req.body.content;
-    newPeep.postedBy = req.user._id;
+    (passport.authenticate('jwt-auth', { sessions: false }, function(err, user) {
+        if(err) 
+            return res.status(500).send({error: 'fail to read database'});
 
-    newPeep.save(function(err) {
-        if (err) {
-            res.json({"error": err});
-            return;
+        if(user == false) {
+            return res.status(401).send({error: 'invalid access token'})
         }
-        res.json({"success": true});
-    });
+
+        newPeep = new Peep();
+        newPeep.content = req.body.content;
+        newPeep.postedBy = user._id;
+    
+        newPeep.save(function(err) {
+            if (err) {
+                res.json({"error": err});
+                return;
+            }
+            res.json({"success": true});
+        });
+    }))(req, res, next)
 });
 
 router.get('/', function(req, res, next){
-    Peep.find().populate('postedBy').exec(function(err, peeps){
+    (passport.authenticate('jwt-auth', { sessions: false }, function(err, user) {
         if(err) 
             return res.status(500).send({error: 'fail to read database'});
-        res.json(peeps);
-    })
+        
+        if(user == false) {
+            return res.status(401).send({error: 'invalid access token'})
+        }
+
+        Peep.find().populate('postedBy').exec(function(err, peeps){
+            if(err) 
+                return res.status(500).send({error: 'fail to read database'});
+            res.json(peeps);
+        })
+    }))(req, res, next)
 });
 
 module.exports = router;
